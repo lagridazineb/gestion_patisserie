371ab1f837d6956cc7b36b402013a662534505f0
lagridazineb <lagridazineb@gmail.com>
2026-07-21 22:01:20 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -54,6 +54,7 @@ app.use('/api/refunds', require('./routes/refunds'))
 app.use('/api/purchases', require('./routes/purchases'))
 app.use('/api/fonds-caisse', require('./routes/fondsCaisse'))
 app.use('/api/rziza', require('./routes/rziza'))
+app.use('/api/retours', require('./routes/retours'))
 app.use('/api/bilan', require('./routes/bilan'))
 app.use('/api/sessions', require('./routes/sessions'))
 
