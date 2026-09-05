2692f333bc6d4d9e94e6907f55e6cd847d4bd648
lagridazineb <lagridazineb@gmail.com>
2026-09-05 23:37:38 +0100

    Create cafeInfo.js

---
+++ client/src/data/cafeInfo.js
@@ -0,0 +1,6 @@
+// Informations affichées sur le reçu (ticket de vente) de l'espace Café.
+export const CAFE_INFO = {
+  name: 'Dianna Café',
+  address: 'Av. Mohammed V, Hay Karima, Salé',
+  phone: '05 37 86 28 80',
+}
